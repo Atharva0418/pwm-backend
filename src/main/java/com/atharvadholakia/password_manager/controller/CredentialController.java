67f4edaaf1e9a9a3d3ac6ae7225e8e52665157d1
Atharva0418 <atharvadholakia2004@gmail.com>
2024-09-06 14:37:06 +0530

    Added dev profile for logging

--- src/main/java/com/atharvadholakia/password_manager/controller/CredentialController.java
+++ src/main/java/com/atharvadholakia/password_manager/controller/CredentialController.java
@@ -4,6 +4,8 @@ import com.atharvadholakia.password_manager.data.Credential;
 import com.atharvadholakia.password_manager.service.CredentialService;
 import jakarta.validation.Valid;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/credentials")
 public class CredentialController {
+
+  private static final Logger logger = LoggerFactory.getLogger(CredentialController.class);
 
   private final CredentialService credentialService;
 
@@ -20,9 +24,19 @@ public class CredentialController {
 
   @PostMapping
   public ResponseEntity<Credential> createCredential(@Valid @RequestBody Credential credential) {
+    logger.debug(
+        "Creating Credential with Servicename: {}, Username: {}, Password: {}.",
+        credential.getServiceName(),
+        credential.getUsername(),
+        credential.getPassword());
     Credential createdCredential =
         credentialService.createCredential(
             credential.getServiceName(), credential.getUsername(), credential.getPassword());
+    logger.info(
+        "Created Credential with Servicename: {}, Username: {}, Password: {}.",
+        credential.getServiceName(),
+        credential.getUsername(),
+        credential.getPassword());
     return new ResponseEntity<>(createdCredential, HttpStatus.CREATED);
   }
 
