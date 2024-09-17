74c197b2793953dea5d2f10091b379c35d5c7a99
Atharva0418 <atharvadholakia2004@gmail.com>
2024-09-17 23:24:13 +0530

    One more try

--- src/main/java/com/atharvadholakia/password_manager/repository/CredentialRepository.java
+++ src/main/java/com/atharvadholakia/password_manager/repository/CredentialRepository.java
@@ -2,5 +2,7 @@ package com.atharvadholakia.password_manager.repository;
 
 import com.atharvadholakia.password_manager.data.Credential;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CredentialRepository extends JpaRepository<Credential, String> {}
