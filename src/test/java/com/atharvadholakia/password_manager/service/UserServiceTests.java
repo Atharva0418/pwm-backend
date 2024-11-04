8203872d263203ad0cb420edbff24b0d150d9ebc
Atharva0418 <atharvadholakia2004@gmail.com>
2024-11-04 19:16:17 +0530

    Removed unnecessary imports in UserServiceTests

--- src/test/java/com/atharvadholakia/password_manager/service/UserServiceTests.java
+++ src/test/java/com/atharvadholakia/password_manager/service/UserServiceTests.java
@@ -1,7 +1,6 @@
 package com.atharvadholakia.password_manager.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
