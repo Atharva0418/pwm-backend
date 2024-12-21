9cabef8fb3255e563a9b48e6f4e5fb782a2b2592
Atharva0418 <atharvadholakia2004@gmail.com>
2024-12-21 20:13:46 +0530

    Added a scheduled health checkup every 12 minutes.

--- src/main/java/com/atharvadholakia/password_manager/PasswordManagerApplication.java
+++ src/main/java/com/atharvadholakia/password_manager/PasswordManagerApplication.java
@@ -2,12 +2,13 @@ package com.atharvadholakia.password_manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PasswordManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PasswordManagerApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(PasswordManagerApplication.class, args);
+  }
 }
