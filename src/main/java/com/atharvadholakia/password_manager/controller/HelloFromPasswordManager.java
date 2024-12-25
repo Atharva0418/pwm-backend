841bb7b980cb9efe75524c128ab5430904f3f132
Atharva0418 <atharvadholakia2004@gmail.com>
2024-12-25 13:49:45 +0530

    Trying a new healthcheckup to keep the server active on render.

--- src/main/java/com/atharvadholakia/password_manager/controller/HelloFromPasswordManager.java
+++ src/main/java/com/atharvadholakia/password_manager/controller/HelloFromPasswordManager.java
@@ -1,5 +1,7 @@
 package com.atharvadholakia.password_manager.controller;
 
+import com.atharvadholakia.password_manager.data.Credential;
+import com.atharvadholakia.password_manager.service.CredentialService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class HelloFromPasswordManager {
 
+  private final CredentialService credentialService;
+
+  public HelloFromPasswordManager(CredentialService credentialService) {
+    this.credentialService = credentialService;
+  }
+
   @RequestMapping(
       value = "/",
       method = {RequestMethod.GET, RequestMethod.HEAD})
@@ -20,7 +28,13 @@ public class HelloFromPasswordManager {
 
   @RequestMapping(value = "/health", method = RequestMethod.HEAD)
   public ResponseEntity<Void> healthCheck() {
-    log.info("Server is running.");
+    log.info("Health Checkup start.");
+    Credential credential =
+        credentialService.createCredential("TestServiceName@1", "TestUserName@1", "TestPassword@1");
+
+    credentialService.deleteCredentialById(credential.getId());
+
+    log.info("Health Checkup finish.");
     return new ResponseEntity<>(HttpStatus.OK);
   }
 }
